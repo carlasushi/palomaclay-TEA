3335d1ca40bfa5913037043b18f1abb37b608c51
Carla Paloma <cpcodes@pm.me>
2022-03-30 08:04:53 -0700

    nav description

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,11 +1,10 @@
 module.exports = [
  
-  { name: 'week 1', route: '/week-1/' },
-  { name: 'week 2', route: '/week-2/' },
-  { name: 'week 3', route: '/week-3/' },
-  { name: 'week 4', route: '/week-4/' }, 
-  { name: 'week 5', route: '/week-5/' }, 
-  { name: 'week 6', route: '/week-6/' },
-
+  { name: 'week 1', info: " | Centering + Wedging", route: '/week-1/' },
+  { name: 'week 2', info: "| Centering + Handbuilding", route: '/week-2/' },
+  { name: 'week 3', info: "| Centering + Trimming", route: '/week-3/' },
+  { name: 'week 4', info: "| Trimming + Textures", route: '/week-4/' }, 
+  { name: 'week 5', info: "| Glazing", route: '/week-5/' }, 
+  { name: 'week 6', info: "| Wrapping Up", route: '/week-6/' },
   { name: 'About', route: '/about/' },
 ]
